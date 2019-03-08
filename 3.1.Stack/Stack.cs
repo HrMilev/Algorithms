3d1bd80569ce51d0a0524d0b0d6f9ac2cd0a934d
Hristo Milev <43407775+HrMilev@users.noreply.github.com>
2019-03-08 16:45:50 +0200

    Add files via upload

---
+++ 3.1.Stack/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class Stack<T>
+{
+    private T[] stackArray;
+    private int pointer;
+
+    public Stack()
+    {
+        this.stackArray = new T[4];
+        this.pointer = -1;
+    }
+
+    public void Push(T item)
+    {
+        if (pointer == stackArray.Length-1)
+        {
+            T[] temp = new T[pointer * 2];
+            Array.Copy(stackArray, temp,stackArray.Length);
+            stackArray = temp;
+        }
+        pointer++;
+        stackArray[pointer] = item;
+    }
+
+    public T Peek()
+    {
+        if (pointer<0)
+        {
+            throw new Exception("Stack is empty");
+        }
+        else
+        {
+            return stackArray[pointer];
+        }
+    }
+
+    public T Pop()
+    {
+        if (pointer < 0)
+        {
+            throw new Exception("Stack is empty");
+        }
+        else
+        {
+            pointer--;
+            return stackArray[pointer+1];
+        }
+
+    }
+
+}
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack<int>();
+        for (int i = 1; i < 8; i++)
+        {
+            stack.Push(i);
+        }
+        Console.WriteLine(stack.Peek());
+        for (int i = 1; i < 8; i++)
+        {
+            stack.Pop();
+        }
+        Console.WriteLine(stack.Peek());
+
+    }
+}
